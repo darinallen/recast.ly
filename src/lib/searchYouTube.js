45abf297b3fd75fe27f2f1889294c83b895e157d
Darin Allen <darinallen@gmail.com>
2017-02-25 17:31:52 -0500

    searchYouTube tests partially passing

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,24 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  $.ajax({
+  // This is the url you should use to communicate with the parse API server.
+    url: 'https://www.googleapis.com/youtube/v3/search?' + 'maxResults=' + options.max + '&q=' + options.query + '&key=' + options.key,
+    type: 'GET',
+    // data: { order: '-createdAt' },
+    contentType: 'application/json',
+    //headers: {"Content-Security-Policy": 'default-src "none"; script-src "self"; connect-src "self"; img-src "self"; style-src "self"'},
+
+    success: function(videos) {
+      console.log('videos came in');
+      // var videos = videos.results;
+      // videos.forEach(function(video) {
+      //   // do something
+      // });
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('recast.ly: Failed to receive videos', data);
+    }
+  });
 };
 
 window.searchYouTube = searchYouTube;
